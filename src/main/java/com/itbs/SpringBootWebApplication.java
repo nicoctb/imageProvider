7aea6806ce93ae8d6b793d5f0c147b01f6c330cd
Octavian Boiciu <nicoctb@yahoo.com>
2017-12-08 12:48:41 +0200

    added Mongo autoconfig exclude at app start

--- src/main/java/com/itbs/SpringBootWebApplication.java
+++ src/main/java/com/itbs/SpringBootWebApplication.java
@@ -1,11 +1,14 @@
 package com.itbs;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class})
 public class SpringBootWebApplication extends SpringBootServletInitializer {
 
     @Override
